098adae888206785cdaa3e8dbd0735797b2db8ac
Aareon Sullivan <askully13@gmail.com>
2024-08-24 15:31:12 -0500

    Use correct parent when centering

--- turtlelauncher/dialogs/game_launch.py
+++ turtlelauncher/dialogs/game_launch.py
@@ -1,11 +1,12 @@
 from turtlelauncher.dialogs.base import BaseDialog
-from PySide6.QtWidgets import QProgressBar, QLabel
-from PySide6.QtCore import Qt, QTimer
+from PySide6.QtWidgets import QProgressBar, QLabel, QApplication
+from PySide6.QtCore import Qt, QTimer, QPoint
 from pathlib import Path
 from loguru import logger
 
 class GameLaunchDialog(BaseDialog):
     def __init__(self, parent=None):
+        self.master = parent
         icon_path = Path(__file__).parent.parent.parent / "assets" / "images" / "turtle_wow_icon.png"
         super().__init__(
             parent=parent,
@@ -59,10 +60,34 @@ class GameLaunchDialog(BaseDialog):
 
     def debug_visibility(self):
         logger.debug(f"GameLaunchDialog visibility: {self.isVisible()}, Geometry: {self.geometry()}")
-        if self.parent():
-            logger.debug(f"Parent geometry: {self.parent().geometry()}")
+        parent = self.master
+        if parent is not None:
+            logger.debug(f"Parent geometry: {parent.geometry()}")
+        else:
+            logger.debug("No parent widget")
 
     def closeEvent(self, event):
         logger.info("GameLaunchDialog closeEvent triggered")
         self.debug_timer.stop()
         super().closeEvent(event)
+
+    def showEvent(self, event):
+        super().showEvent(event)
+        self.center_on_parent()
+
+    def center_on_parent(self):
+        parent = self.master
+        if parent is not None:
+            parent_rect = parent.geometry()
+            self_rect = self.geometry()
+            
+            new_x = parent_rect.center().x() - self_rect.width() // 2
+            new_y = parent_rect.center().y() - self_rect.height() // 2
+            
+            new_pos = parent.mapToGlobal(QPoint(new_x, new_y))
+            self.move(new_pos)
+            logger.debug(f"Centered dialog at: {new_pos}")
+        else:
+            logger.debug("No parent widget, centering on screen")
+            screen = QApplication.primaryScreen().geometry()
+            self.move(screen.center() - self.rect().center())
