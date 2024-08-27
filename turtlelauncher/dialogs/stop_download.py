5841c4c33764bb1eb772c356b0cd2bbf28a74aba
Aareon Sullivan <askully@amazon.com>
2024-08-27 17:13:30 -0500

    Use globals module, internationalization

--- turtlelauncher/dialogs/stop_download.py
+++ turtlelauncher/dialogs/stop_download.py
@@ -1,22 +1,22 @@
 from PySide6.QtWidgets import QHBoxLayout
-from pathlib import Path
 from turtlelauncher.dialogs.base import BaseDialog
 from loguru import logger
+from turtlelauncher.utils.globals import IMAGES
 
 
 class StopDownloadDialog(BaseDialog):
     def __init__(self, parent=None):
-        icon_path = Path(__file__).parent.parent.parent / "assets" / "images" / "turtle_wow_icon.png"
+        icon_path = IMAGES / "turtle_wow_icon.png"
         super().__init__(
             parent=parent,
-            title="Stop Download",
+            title=self.tr("Stop Download"),
             icon_path=icon_path,
             modal=True,
             custom_styles=self.get_custom_styles()
         )
 
-        self.add_message("Are you sure you want to stop the download?", color="#FFD700")  # Gold color
-        self.add_message("The download progress will be lost.", color="#FF69B4")  # Hot pink color
+        self.add_message(self.tr("Are you sure you want to stop the download?"), color="#FFD700")  # Gold color
+        self.add_message(self.tr("The download progress will be lost."), color="#FF69B4")  # Hot pink color
 
         self.setup_buttons()
 
@@ -24,8 +24,8 @@ class StopDownloadDialog(BaseDialog):
         button_layout = QHBoxLayout()
         button_layout.setSpacing(10)
 
-        self.create_button("Yes, Stop", self.accept, button_layout, "dialog-button")
-        self.create_button("No, Continue", self.reject, button_layout, "dialog-button")
+        self.create_button(self.tr("Yes, Stop"), self.accept, button_layout, "dialog-button")
+        self.create_button(self.tr("No, Continue"), self.reject, button_layout, "dialog-button")
 
         self.content_layout.addLayout(button_layout)
 
@@ -45,7 +45,8 @@ class StopDownloadDialog(BaseDialog):
 
     def showEvent(self, event):
         super().showEvent(event)
-        if "Yes, Stop" in self.buttons:
-            self.buttons["Yes, Stop"].setFocus()
+        # Must translate for check, as the button text was likely translated
+        if self.tr("Yes, Stop") in self.buttons:
+            self.buttons[self.tr("Yes, Stop")].setFocus()
         else:
             logger.warning("'Yes, Stop' button not found in buttons dictionary")
